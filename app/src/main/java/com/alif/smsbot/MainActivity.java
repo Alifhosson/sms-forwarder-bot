660a5a5e396e6ede0e6a76496f52147bd388121f
Alif Hosson <153631607+Alifhosson@users.noreply.github.com>
2026-02-26 01:06:19 +0600

    Update MainActivity.java

--- app/src/main/java/com/alif/smsbot/MainActivity.java
+++ app/src/main/java/com/alif/smsbot/MainActivity.java
@@ -1,18 +1,22 @@
 package com.alif.smsbot;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.graphics.Color;
+import android.view.Gravity;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
         TextView tv = new TextView(this);
-        tv.setText("✅ SMS Forwarder is ACTIVE");
+        tv.setText("✅ SMS Forwarder is Running\n\nKeep this app in background.");
         tv.setTextSize(20);
         tv.setTextColor(Color.GREEN);
-        tv.setPadding(50, 100, 50, 0);
+        tv.setGravity(Gravity.CENTER);
+        
         setContentView(tv);
     }
 }
